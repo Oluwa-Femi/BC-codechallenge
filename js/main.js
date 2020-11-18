c04236d1575965456c6270cb53eb171ab205201d
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-11-18 03:59:48 +0000

    Main section styled

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ data.nodes.forEach(proj => {
       ${proj.description ? "<p>" + proj.description + "</p>" : ""}
       <div class="bottom-repo-list">
         <div class="item-prop" style="${!proj.languages.edges[0] ? "display: none;" : ""}">
-        <span class="notch" style="background: ${proj.languages.edges[0]
+        <span class="bott-links" style="background: ${proj.languages.edges[0]
           ? proj.languages.edges[0].node.color
           : ""}">
             </span>
@@ -102,7 +102,7 @@ data.nodes.forEach(proj => {
     </div>
     <div class="star">
       <button>
-        <ion-icon name="star-outline" class="mr-1"></ion-icon> <span> Star </span>
+      <svg class="octicon octicon-star text-gray-light" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg> <span class="star-text"> Star </span>
       </button>
     </div>
   </div>
